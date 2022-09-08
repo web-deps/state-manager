43225bf69eae5059cb58bcb6ff69cac037c3299d
Mishieck Mwale <57598264+Mishieck@users.noreply.github.com>
2022-09-08 17:05:05 +0000

    test: test instantiation and state transition of StateManager - Instantiate StateManager with 'states' option in options and check if initial state is correct. - Instantiate and then change state of set manager. - Check if the states are correctly changed.

---
+++ lib/state-managers/state-manager/state-manager.test.ts
@@ -0,0 +1,44 @@
+import { describe, it, expect } from 'vitest';
+import StateManager from './state-manager';
+
+describe('StateManager class', () => {
+  it('initializes StateManager with "states" option', () => {
+    const color = new StateManager({
+      initialState: 'red',
+      states: [
+        { name: 'red' },
+        {
+          name: 'blue',}
+      ]
+    });
+
+    expect(color.current).toBe('red');
+  });
+
+  it('sets StateManager with initial state "red" and then to "blue"', () => {
+    let currentColor = 'red';
+
+    const color = new StateManager({
+      initialState: currentColor,
+      states: [
+        { name: 'red' },
+        {
+          name: 'blue',
+          observers: [
+            (color) => {
+              currentColor = color;
+            }
+          ]
+        }
+      ]
+    });
+
+    expect(color.current).toBe('red');
+    color.current = 'blue';
+    expect(color.current).toBe('blue');
+    expect(currentColor).toBe('blue');
+    color.current = 'red';
+    expect(color.current).toBe('red');
+    expect(currentColor).toBe('blue');
+  });
+});
