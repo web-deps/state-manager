24a425c554437bf12f6e5bba661ce7329cd88afd
Mishieck Mwale <57598264+Mishieck@users.noreply.github.com>
2022-09-19 14:43:58 +0000

    feat: add main module

---
+++ lib/main.ts
@@ -0,0 +1,50 @@
+export {
+  StateManager,
+  StateEvent
+} from "./state-managers/state-manager/state-manager";
+
+export {
+  DataStateManager,
+  DataStateEvent
+} from "./state-managers/data-state-manager/data-state-manager";
+
+export {
+  CollectionStateManager,
+  CollectionStateEvent
+} from "./state-managers/collection-state-manager/collection-state-manager";
+
+export type {
+  StateManagerInterface,
+  StateEventInterface,
+  StateOptionInterface,
+  StateObserverInterface,
+  StateContextOptionsInterface,
+  StateManagerOptionsInterface,
+  StateTransitionsInterface,
+  SuspenseHandlerType
+} from "./state-managers/state-manager/state-manager";
+
+export type {
+  DataStateManagerInterface,
+  DataStateEventInterface,
+  DataStateOptionInterface,
+  DataStateOptionsInterface,
+  DataStateObserverInterface,
+  DataTesterInterface,
+  DataTestItemInterface,
+  DataUpdateHandlerInterface,
+  DataStateContextOptionsInterface,
+  DataStateSuspenseHandlerType
+} from "./state-managers/data-state-manager/data-state-manager";
+
+export type {
+  CollectionStateManagerInterface,
+  CollectionStateEventInterface,
+  CollectionStateObserverInterface,
+  CombinationMatcherInterface,
+  CollectionStateOptionInterface,
+  CollectionStateSuspenseHandlerInterface,
+  CollectionStateContextOptionsInterface,
+  CollectionStateOptionsInterface,
+  CollectionStateTransitionsInterface
+} from "./state-managers/collection-state-manager/collection-state-manager";
