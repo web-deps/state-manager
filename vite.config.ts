a6ed3df429bc0ff6a181442d9435871222ed65c2
Mishieck Mwale <57598264+Mishieck@users.noreply.github.com>
2022-09-19 15:40:07 +0000

    core: set correct project name and description

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import path from 'path'
-import {defineConfig} from 'vite'
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'lib/main.js'),
-      name: 'cewr',
-      fileName: (format) => `cewr.${format}.js`
+      entry: path.resolve(__dirname, "lib/main.js"),
+      name: "state-manager",
+      fileName: (format) => `state-manager.${format}.js`
     }
   }
 });
