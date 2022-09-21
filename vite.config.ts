255c3d3e6a557456a83da0c26e5247d6a7aecddf
Mishieck Mwale <57598264+Mishieck@users.noreply.github.com>
2022-09-21 16:23:08 +0000

    build: update vite congig - Change name of library. - Add vite-plugin-dts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import path from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "lib/main.js"),
-      name: "state-manager",
+      entry: path.resolve(__dirname, "lib/main.ts"),
+      name: "stateManager",
       fileName: (format) => `state-manager.${format}.js`
     }
-  }
+  },
+  plugins: [
+    dts({
+      insertTypesEntry: true
+    })
+  ]
 });
